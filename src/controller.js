1300fcd057de80c03cc09475265edb553647df0a
Miko Kari <miko.kari@outlook.com>
2020-12-09 17:13:45 +0200

    added hashing

--- src/controller.js
+++ src/controller.js
@@ -1,7 +1,12 @@
+const hash = require("object-hash");
+
 module.exports = {
   create: function (req, res) {
     const url = req.body.url;
     console.log("CREATING URL from", url);
+    const ms = new Date().getTime();
+    const hashed = hash(url + ms);
+    console.log("hash result:", hashed.substring(0, 16));
 
     res.send("SHORTENED");
   },
