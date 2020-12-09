f92ede71a911bde12173f32d281e006f7ab5f35f
Miko Kari <miko.kari@outlook.com>
2020-12-09 18:19:31 +0200

    added POST route model creation

--- src/controller.js
+++ src/controller.js
@@ -1,14 +1,26 @@
 const hash = require("object-hash");
+let Shortened = require("./models/shortened.model");
 
 module.exports = {
   create: function (req, res) {
     const url = req.body.url;
-    console.log("CREATING URL from", url);
     const ms = new Date().getTime();
-    const hashed = hash(url + ms);
-    console.log("hash result:", hashed.substring(0, 16));
+    const hashed = hash(url + ms).substring(0, 16);
 
-    res.send("SHORTENED");
+    const input = {
+      hash: hashed,
+      original: url,
+    };
+
+    let shortened = new Shortened(input);
+    shortened
+      .save()
+      .then((shortened) => {
+        res.status(200).json(shortened);
+      })
+      .catch((err) => {
+        res.status(400).send("url shortening failed");
+      });
   },
 
   findById: function (req, res) {
