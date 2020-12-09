5632b381d9280b7fd23dc31caf1e5cb1b3deb036
Miko Kari <miko.kari@outlook.com>
2020-12-09 16:26:28 +0200

    added body parser

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+
 const app = express();
 const port = 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
+app
+  .get("/", (req, res) => {
+    res.send("Hello");
+  })
+  .post("/", (req, res) => {
+    console.log("Req body", req.body);
+    res.send("Hello POST");
+  });
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
