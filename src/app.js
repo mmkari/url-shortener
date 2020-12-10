ab49b093b1457bb3019549a4ec714edd57bdecc0
Miko Kari <miko.kari@outlook.com>
2020-12-10 13:25:19 +0200

    changed to default port

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 const router = require("./router");
 require("./models"); // connect to DB
