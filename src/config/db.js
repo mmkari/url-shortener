fd079e21c3c63d136fa41bc23fe55628600360df
Miko Kari <miko.kari@outlook.com>
2020-12-10 16:50:48 +0200

    add different DB for tests

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,18 @@
 const dbUser = "dbUser";
 const dbPassword = "dbUserPw";
 const dbName = "shortUrls";
+const dbNameTests = "shortUrlsTests";
 
-const MONGODB_URI = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.bhoih.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+let resolvedDbName;
+switch (process.env.NODE_ENV) {
+  case "test":
+    resolvedDbName = dbNameTests;
+    break;
+  default:
+    resolvedDbName = dbName;
+    break;
+}
+
+const MONGODB_URI = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.bhoih.mongodb.net/${resolvedDbName}?retryWrites=true&w=majority`;
 
 module.exports = MONGODB_URI;
