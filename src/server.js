6053351127ce97b9ab19949735d32879496ae4b9
Miko Kari <miko.kari@outlook.com>
2020-12-10 18:23:46 +0200

    added server close

--- src/server.js
+++ src/server.js
@@ -7,3 +7,12 @@ const app = createApp();
 const server = app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
+
+const closeServer = () => {
+  server.close(() => {
+    console.log("Closed down server");
+  });
+};
+
+process.on("SIGINT", closeServer);
+process.on("SIGTERM", closeServer);
