3f2c6ebc2cfd3c632fa7d55a74882a161b94cff6
Miko Kari <miko.kari@outlook.com>
2020-12-11 15:14:10 +0200

    added missing/invalid url validation

--- src/controller.js
+++ src/controller.js
@@ -2,26 +2,42 @@ const hash = require("object-hash");
 let Shortened = require("./models/shortened.model");
 const URL_BASE = "http://localhost";
 
+const INVALID_CHARACTERS_REGEXP = /[ "<>\^`{|}]/;
+const isUrlInvalid = (url) =>
+  ((url || "").match(INVALID_CHARACTERS_REGEXP) || []).length;
+
 module.exports = {
   create: function (req, res) {
-    const url = req.body.url;
-    const ms = new Date().getTime();
-    const hashed = hash(url + ms).substring(0, 16);
+    try {
+      const url = req.body.url;
 
-    const input = {
-      hash: hashed,
-      original: url,
-    };
+      // validation
+      // 1. must have url param
+      // 2. url cannot contain invalid characters
+      if (!url || isUrlInvalid(url)) {
+        return res.status(400).send(); // RETURN early with 400
+      }
 
-    let shortened = new Shortened(input);
-    shortened
-      .save()
-      .then((shortened) => {
-        res.status(200).send(`${URL_BASE}/${hashed}`);
-      })
-      .catch(() => {
-        res.status(400).send();
-      });
+      const ms = new Date().getTime();
+      const hashed = hash(url + ms).substring(0, 16);
+
+      const input = {
+        hash: hashed,
+        original: url,
+      };
+
+      let shortened = new Shortened(input);
+      shortened
+        .save()
+        .then((shortened) => {
+          res.status(200).send(`${URL_BASE}/${hashed}`);
+        })
+        .catch(() => {
+          res.status(500).send();
+        });
+    } catch (error) {
+      console.log("ERROR IN POST CONTROLLER:", error);
+    }
   },
 
   findById: function (req, res) {
@@ -38,7 +54,7 @@ module.exports = {
           res.status(404).send();
         }
       })
-      .catch(() => {
+      .catch((err) => {
         res.status(500).send();
       });
   },
